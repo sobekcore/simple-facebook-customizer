f3eb06e920d52599c854616a6c4241fb2583e993
sobekcore <sobekcore@gmail.com>
2022-01-12 00:43:41 +0100

    Add sidebars margin settings and toggles

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,42 @@
 const RIGHT_BAR_SELECTOR = ".rq0escxv.lpgh02oy.du4w35lb.o387gat7.qbu88020.pad24vr5.rirtxc74.dp1hu0rb.fer614ym.ni8dbmo4.stjgntxs.rek2kq2y.be9z9djy.bx45vsiw";
+const LEFT_BAR_SELECTOR = ".rq0escxv.du4w35lb.o387gat7.qbu88020.pad24vr5.rirtxc74.dp1hu0rb.fer614ym.ni8dbmo4.stjgntxs.lpgh02oy.be9z9djy.hlyrhctz";
 
-const defineInitialValues = (overwrite, element) => {
+const options = [
+  {
+    name: "rightSidebar",
+    selector: RIGHT_BAR_SELECTOR,
+  },
+  {
+    name: "rightMargin",
+    selector: RIGHT_BAR_SELECTOR,
+  },
+  {
+    name: "leftSidebar",
+    selector: LEFT_BAR_SELECTOR,
+  },
+  {
+    name: "leftMargin",
+    selector: LEFT_BAR_SELECTOR,
+  }
+];
+
+const defineOptionValues = (overwrite, element, value = false) => {
   switch (element) {
-    case "rightBarElement":
-      overwrite.style["opacity"] = "100";
-      overwrite.style["margin-left"] = "0";
+    case "rightSidebar":
+      overwrite.style["opacity"] = value ? "0" : "100";
+      overwrite.style["visibility"] = value ? "hidden" : "initial";
+      break;
+    case "rightMargin":
+      overwrite.style["min-width"] = value ? "0" : "280px";
+      overwrite.style["flex-basis"] = value ? "0" : "360px";
+      break;
+    case "leftSidebar":
+      overwrite.style["opacity"] = value ? "0" : "100";
+      overwrite.style["visibility"] = value ? "hidden" : "initial";
+      break;
+    case "leftMargin":
+      overwrite.style["min-width"] = value ? "0" : "280px";
+      overwrite.style["flex-basis"] = value ? "0" : "360px";
       break;
   }
 };
@@ -19,29 +51,31 @@ let frame = window.requestAnimationFrame(() => {
   document.head.appendChild(stylesheet);
   stylesheet = stylesheet.sheet;
 
-  stylesheet.insertRule(`${RIGHT_BAR_SELECTOR} {
-    transition: ease-in-out 0.25s all, ease-in 0.15s opacity !important;
-    margin-left: 0;
-    opacity: 100;
-  }`);
+  let overwrites = {};
 
-  let [ overwrite ] = stylesheet.cssRules;
+  for (let option of options) {
+    stylesheet.insertRule(`${option.selector} {
+      transition: ease-in-out 0.25s all, ease-in 0.10s opacity !important;
+    }`);
 
-  chrome.storage.local.get("rightSidebarHidden", (storage) => {
-    if (storage.rightSidebarHidden) {
-      overwrite.style["opacity"] = "0";
-      overwrite.style["margin-left"] = "-360px";
+    let [ overwrite ] = stylesheet.cssRules;
+    overwrites[option.name] = overwrite;
+
+    chrome.storage.local.get(option.name, (storage) => {
+      if (storage[option.name]) {
+        defineOptionValues(overwrite, option.name, true);
+      } else {
+        defineOptionValues(overwrite, option.name);
+      }
+    });
+  }
+
+  chrome.runtime.onMessage.addListener((message) => {
+    let overwrite = overwrites[message.name];
+    if (message.value) {
+      defineOptionValues(overwrite, message.name, true);
     } else {
-      defineInitialValues(overwrite, "rightBarElement");
-    }
-  });
-
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.hide) {
-      overwrite.style["opacity"] = "0";
-      overwrite.style["margin-left"] = "-360px";
-    } else {
-      defineInitialValues(overwrite, "rightBarElement");
+      defineOptionValues(overwrite, message.name);
     }
   });
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,20 +1,112 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const rightSidebar = document.querySelector("#hide-right-sidebar");
-  let rightSidebarHidden = false;
+const visualizeToggles = (element, loadDataOnly = false) => {
+  let cover = element.previousElementSibling;
+  let toggleSwitch = cover.querySelector(".extenstion-setting-toggle-switch");
 
-  chrome.storage.local.get("rightSidebarHidden", (storage) => {
-    rightSidebarHidden = storage.rightSidebarHidden
-    rightSidebar.checked = rightSidebarHidden;
+  if (loadDataOnly) {
+    cover.style["transition"] = "none";
+    toggleSwitch.style["transition"] = "none";
 
-    let event = new Event("change");
-    event.loadDataOnly = true;
-    rightSidebar.dispatchEvent(event);
+    // Remove inline transitions after next browser tick, this
+    // brings back original transitions from the extension stylesheet.
+    setTimeout(() => {
+      cover.style["transition"] = null;
+      toggleSwitch.style["transition"] = null;
+    });
+  }
+
+  if (element.checked) {
+    toggleSwitch.style["margin-left"] = "22px";
+    cover.style["border-color"] = "#3f51b5"
+    cover.style["background"] = "#3f51b5"
+  } else {
+    toggleSwitch.style["margin-left"] = "0";
+    cover.style["border-color"] = "#c8c8c8";
+    cover.style["background"] = "#c8c8c8";
+  }
+};
+
+const triggerDependent = (toTriggerElement, optionElement) => {
+  toTriggerElement.disabled = !optionElement.checked;
+
+  let toggle = toTriggerElement.parentElement;
+  toggle.setAttribute("data-disabled", !optionElement.checked);
+
+  if (toTriggerElement.disabled && toTriggerElement.checked) {
+    toTriggerElement.checked = false;
+
+    let changeEvent = new Event("change");
+    changeEvent.target = optionElement;
+
+    visualizeToggles(toTriggerElement);
+    toTriggerElement.dispatchEvent(changeEvent);
+  }
+};
+
+const rightSidebarElement = document.querySelector("#right-sidebar");
+const rightMarginElement = document.querySelector("#right-margin");
+const leftSidebarElement = document.querySelector("#left-sidebar");
+const leftMarginElement = document.querySelector("#left-margin");
+
+const options = [
+  {
+    name: "rightSidebar",
+    value: false,
+    element: rightSidebarElement,
+    triggers: ["rightMargin"],
+  },
+  {
+    name: "rightMargin",
+    value: false,
+    element: rightMarginElement,
+  },
+  {
+    name: "leftSidebar",
+    value: false,
+    element: leftSidebarElement,
+    triggers: ["leftMargin"],
+  },
+  {
+    name: "leftMargin",
+    value: false,
+    element: leftMarginElement,
+  }
+];
+
+for (let option of options) {
+  chrome.storage.local.get(option.name, (storage) => {
+    option.value = storage[option.name];
+    option.element.checked = option.value;
+
+    let changeEvent = new Event("change");
+    changeEvent.target = option.element;
+    changeEvent.loadDataOnly = true;
+
+    visualizeToggles(option.element, true);
+    option.element.dispatchEvent(changeEvent);
   });
 
-  rightSidebar.addEventListener("change", (event) => {
+  option.element.addEventListener("click", (event) => {
+    visualizeToggles(event.target);
+  });
+
+  option.element.addEventListener("change", (event) => {
+    const option = options.find(({ element }) => {
+      return element === event.target;
+    });
+
+    if (option.triggers) {
+      for (let trigger of option.triggers) {
+        let toTrigger = options.find(({ name }) => {
+          return name === trigger;
+        });
+
+        triggerDependent(toTrigger.element, option.element);
+      }
+    }
+
     if (!event.loadDataOnly) {
-      rightSidebarHidden = !rightSidebarHidden;
-      chrome.storage.local.set({ "rightSidebarHidden": rightSidebarHidden });
+      option.value = !option.value;
+      chrome.storage.local.set({ [option.name]: option.value });
     }
 
     const params = {
@@ -24,8 +116,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     chrome.tabs.query(params, (tabs) => {
       const [ currentTab ] = tabs;
-      const message = { event: event, hide: rightSidebarHidden };
+      const message = {
+        event: event,
+        name: option.name,
+        value: option.value,
+      };
+
       chrome.tabs.sendMessage(currentTab.id, message);
     });
-  })
-});
+  });
+}
