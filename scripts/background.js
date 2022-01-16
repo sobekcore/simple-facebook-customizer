2ea1a7dd97d66d7e74553f7576f814744caca15d
sobekcore <sobekcore@gmail.com>
2022-01-16 22:24:22 +0100

    Implemented a service worker for changing an icon activity

---
+++ scripts/background.js
@@ -0,0 +1,30 @@
+/**
+ * A regex to match any of the Facebook urls
+ *
+ * @type {RegExp}
+ * @constant
+ */
+const regex = new RegExp(/^https:\/\/\S*\.facebook\.com\/\S*/);
+
+/**
+ * @returns {void}
+ */
+const listenForIconAcivation = () => {
+  chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.url.match(regex)) {
+      const icons = {
+        "16": "/icons/16.png",
+        "32": "/icons/32.png",
+        "48": "/icons/48.png",
+      };
+
+      chrome.action.setIcon({
+        tabId: tabId,
+        path: icons,
+      });
+    }
+  });
+};
+
+// Background script entry point
+listenForIconAcivation();
